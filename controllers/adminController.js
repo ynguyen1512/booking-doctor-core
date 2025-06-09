74f916487d134e9d6eb9545e50bca9fb1eb0ea51
Richard Nguyen <nguyenphuyfgw@gmail.com>
2025-06-09 17:10:52 +0700

    [FEATURE] Change Availability API

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -112,4 +112,21 @@ const adminLogin = async (req, res) => {
   }
 };
 
-export { addDoctor, adminLogin };
+// API for getting doctors list for admin panel
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select('-password')
+    res.json({
+      success: true,
+      doctors
+    })
+  } catch (error) {
+     console.log(error);
+     res.json({
+       success: false,
+       message: error.message,
+     });
+  }
+}
+
+export { addDoctor, adminLogin, allDoctors };
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { addDoctor, adminLogin } from "../controllers/adminController.js";
+import { addDoctor, adminLogin, allDoctors } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
+import { changeAvailability } from "../controllers/doctorController.js";
 
 const adminRouter = express.Router();
 adminRouter.post("/add-doctor",authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", adminLogin);
+adminRouter.post("/all-doctors", authAdmin, allDoctors);
+adminRouter.post("/change-availability", authAdmin, changeAvailability);
 
 export default adminRouter;
