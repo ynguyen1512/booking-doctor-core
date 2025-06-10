b6e3b1457a9ae599eb406d807ddd4ce0dee73a80
Richard Nguyen <nguyenphuyfgw@gmail.com>
2025-06-10 14:07:53 +0700

    [FEATURE] Get & Update User Profile API

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+  getProfile,
+  loginUser,
+  registerUser,
+  updateProfile,
+} from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post(
+  "/update-profile",
+  upload.single("image"),
+  authUser,
+  updateProfile
+);
 
 export default userRouter;
